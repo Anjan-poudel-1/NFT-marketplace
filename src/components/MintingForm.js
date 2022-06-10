fd9c5fe1e70e832d990ac84a62742451d4500d4d
Anjan_poudel <poudelanjan8@gmail.com>
2022-06-10 08:49:27 +0545

    Changes

--- src/components/MintingForm.js
+++ src/components/MintingForm.js
@@ -32,7 +32,7 @@ function MintingForm({mintFunction}) {
             <label htmlFor='image-upload' style={{padding:"0.35rem 2rem ",cursor:"pointer",borderRadius:"10px",backgroundColor:"#353535",color:"white"}}>
                     Upload Image
             </label> */}
-             <input style={{width:"400px",padding:"0.25rem 0.75rem"}} value={imageName} placeholder="Enter image path k1,k2..k11" type="text" onChange={(e)=>uploadImage(e)}/>
+             <input style={{width:"400px",padding:"0.25rem 0.75rem"}} value={imageName} placeholder="Enter image path k1,k2....,k11.png" type="text" onChange={(e)=>uploadImage(e)}/>
             
         {/* <br/> {imageName && imageName} */}
             <br/>{error && error}
